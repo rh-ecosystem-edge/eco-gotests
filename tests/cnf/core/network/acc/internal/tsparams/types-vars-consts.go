8e3cfb8f354385ff6728c70774143b1f6eec9896
Anvesh J <ajaggapa@redhat.com>
2025-10-13 22:57:15 +0530

    cnf network: acc tests

---
+++ tests/cnf/core/network/acc/internal/tsparams/types-vars-consts.go
@@ -0,0 +1,67 @@
+package tsparams
+
+import (
+	"time"
+
+	"github.com/openshift-kni/k8sreporter"
+	mcfgv1 "github.com/openshift/api/machineconfiguration/v1"
+	performanceprofileV2 "github.com/openshift/cluster-node-tuning-operator/pkg/apis/performanceprofile/v2"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/cnf/core/network/internal/netparam"
+)
+
+const (
+	// LabelSuite represents acc label that can be used for test cases selection.
+	LabelSuite = "accelerator"
+	// TestNamespaceName acc namespace where all test cases are performed.
+	TestNamespaceName = "acc-tests"
+	// Acc100DeviceID represents the device id of the acc100.
+	Acc100DeviceID = "0d5c"
+	// Acc200DeviceID represents the device id of the acc200.
+	Acc200DeviceID = "57c0"
+	// Acc100ResourceName represents the resource name of the acc100.
+	Acc100ResourceName = "intel.com/intel_fec_acc100"
+	// Acc100EnvVar represents the env variable of the acc100.
+	Acc100EnvVar = "PCIDEVICE_INTEL_COM_INTEL_FEC_ACC100"
+	// Acc200ResourceName represents the resource name of the acc200.
+	Acc200ResourceName = "intel.com/intel_fec_acc200"
+	// Acc200EnvVar represents the env variable of the acc200.
+	Acc200EnvVar = "PCIDEVICE_INTEL_COM_INTEL_FEC_ACC200"
+	// TotalNumberBbdevTests represents the total number of bbdev tests.
+	TotalNumberBbdevTests = 33
+	// ExpectedNumberBbdevTestsPassedForAcc100 represents the expected number of bbdev tests passed.
+	ExpectedNumberBbdevTestsPassedForAcc100 = 27
+	// ExpectedNumberBbdevTestsPassedForAcc200 represents the expected number of bbdev tests passed.
+	ExpectedNumberBbdevTestsPassedForAcc200 = 14
+)
+
+var (
+	// Labels represents the range of labels that can be used for test cases selection.
+	Labels = append(netparam.Labels, LabelSuite)
+	// WaitTimeout represents timeout for the most ginkgo Eventually functions.
+	WaitTimeout = 3 * time.Minute
+	// RetryInterval represents retry interval for the most ginkgo Eventually functions.
+	RetryInterval = 3 * time.Second
+	// MCOWaitTimeout represent timeout for mco operations.
+	MCOWaitTimeout = 60 * time.Minute
+	// ReporterCRDsToDump tells to the reporter what CRs to dump.
+	ReporterCRDsToDump = []k8sreporter.CRData{
+		{Cr: &performanceprofileV2.PerformanceProfileList{}},
+		{Cr: &mcfgv1.MachineConfigPoolList{}},
+	}
+	// OperatorNamespace represents the namespace of the fec operator.
+	OperatorNamespace = "vran-acceleration-operators"
+	// DaemonsetNames represents the names of the daemonsets to check for fec operator.
+	DaemonsetNames = []string{"accelerator-discovery", "sriov-device-plugin", "sriov-fec-daemonset"}
+	// ReporterNamespacesToDump tells to the reporter from where to collect logs.
+	ReporterNamespacesToDump = map[string]string{
+		"openshift-performance-addon-operator": "performance",
+		TestNamespaceName:                      "other",
+	}
+)
+
+// VFIOToken represents the vfiotoken struct.
+type VFIOToken struct {
+	Extra struct {
+		VfioToken string `json:"VFIO_TOKEN"`
+	} `json:"extra"`
+}
