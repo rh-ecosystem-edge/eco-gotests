8241367604ad1e17ceafe1411fec6d9a976a80c5
Itzik Ezra <iezra@redhat.com>
2025-11-12 14:12:21 +0200

    revert deletion of module label verification

--- tests/hw-accel/kmm/1upgrade/tests/upgrade-test.go
+++ tests/hw-accel/kmm/1upgrade/tests/upgrade-test.go
@@ -181,6 +181,13 @@ var _ = Describe("KMM", Ordered, Label(tsparams.LabelSuite), func() {
 			By("Await operator to be upgraded")
 			err = await.OperatorUpgrade(APIClient, ModulesConfig.UpgradeTargetVersion, 5*time.Minute)
 			Expect(err).ToNot(HaveOccurred(), "failed awaiting subscription upgrade")
+			// Skip module verification for KMM-HUB since no module was deployed on hub
+			if !check.IsKMMHub() {
+				By("Check module label is still set on nodes after upgrade")
+				_, err = check.NodeLabel(APIClient, moduleName, upgradeTestNamespace,
+					GeneralConfig.WorkerLabelMap)
+				Expect(err).ToNot(HaveOccurred(), "module labels should remain after upgrade")
+			}
 		})
 	})
 })
