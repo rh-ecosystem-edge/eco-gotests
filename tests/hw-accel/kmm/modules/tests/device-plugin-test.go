f4279d3a7861c4574ce49383c08650b23e505f59
ybrodsky-rh <ybrodsky@redhat.com>
2025-12-09 11:01:22 +0200

    Add module redeploy test case (ID: 82674) (#981)
    * Add module redeploy test case (ID: 82674)
    This test validates that a KMM module can be successfully
    redeployed after being deleted along with its namespace.
    
    Test steps:
    1. Deploy module and all resources
    2. Delete only the module
    3. Delete all other resources (configmap, serviceaccount, CRB)
    4. Delete namespace
    5. Recreate namespace
    6. Reapply all resources
    7. Verify module is loaded
    8. Cleanup
    
    Label: redeploy
    
    * Integrate module redeploy logic into device-plugin-test
    
    - Add redeploy test steps (ID: 82674) to existing device-plugin-test (ID: 53678)
    - Add 'redeploy' label to test Context
    - Delete standalone module-redeploy-test.go file
    - Test now validates: deploy → verify → teardown → redeploy → verify
    - Remove redundant Step 8 (cleanup handled by AfterEach)
    - Fix error messages in AfterEach cleanup
    - Add BeforeEach with 2s wait for namespace readiness
    - Poll for namespace deletion before recreating (Step 4)
    
    * Restructure device-plugin-test: move setup to BeforeEach, split into two It blocks
    
    - Move all resource creation to BeforeEach (NS, SA, CRB, CM, KernelMapping, ModuleLoaderContainer, DevicePlugin)
    - Move all cleanup to AfterEach (Module, CM, SA, CRB, NS)
    - Add namespace deletion polling in AfterEach to prevent 'namespace terminating' errors
    - Split into two separate It blocks for deploy (ID: 53678) and redeploy (ID: 82674)
    - Redeploy test runs implicitly via BeforeEach recreating resources after first test cleanup
    - Use closure-scoped variables to share state between BeforeEach/It/AfterEach
    - Eliminate code duplication - both It blocks are identical
    - Add v1beta1 import for proper type definitions
    
    * Fix nlreturn linter error: add blank line before return statement
    
    * Fix typo: rename devicePluginContainerCfd to devicePluginContainerCfg
    
    - Rename variable to match moduleLoaderContainerCfg naming convention
    - Updated all 4 occurrences (declaration + 3 references)

--- tests/hw-accel/kmm/modules/tests/device-plugin-test.go
+++ tests/hw-accel/kmm/modules/tests/device-plugin-test.go
@@ -9,7 +9,9 @@ import (
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/configmap"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/kmm"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/namespace"
+	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/rbac"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/reportxml"
+	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/schemes/kmm/v1beta1"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/serviceaccount"
 
 	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/kmm/internal/await"
@@ -24,63 +26,52 @@ import (
 
 var _ = Describe("KMM", Ordered, Label(kmmparams.LabelSuite, kmmparams.LabelSanity), func() {
 
-	Context("Module", Label("devplug", "kmm-short"), func() {
+	Context("Module", Label("devplug", "kmm-short", "redeploy"), func() {
 
-		moduleName := kmmparams.DevicePluginTestNamespace
-		kmodName := "devplug"
-		serviceAccountName := "devplug-manager"
-		image := fmt.Sprintf("%s/%s/%s:$KERNEL_FULL_VERSION",
-			tsparams.LocalImageRegistry, kmmparams.DevicePluginTestNamespace, kmodName)
+		var (
+			moduleName         = kmmparams.DevicePluginTestNamespace
+			kmodName           = "devplug"
+			serviceAccountName = "devplug-manager"
+			image              = fmt.Sprintf("%s/%s/%s:$KERNEL_FULL_VERSION",
+				tsparams.LocalImageRegistry, kmmparams.DevicePluginTestNamespace, kmodName)
+			buildArgValue = fmt.Sprintf("%s.o", kmodName)
 
-		buildArgValue := fmt.Sprintf("%s.o", kmodName)
+			testNamespace            *namespace.Builder
+			dockerfileConfigMap      *configmap.Builder
+			svcAccount               *serviceaccount.Builder
+			crb                      rbac.ClusterRoleBindingBuilder
+			moduleLoaderContainerCfg *v1beta1.ModuleLoaderContainerSpec
+			devicePluginContainerCfg *v1beta1.DevicePluginContainerSpec
+		)
 
-		AfterEach(func() {
-			By("Delete Module")
-			_, err := kmm.NewModuleBuilder(APIClient, moduleName, kmmparams.DevicePluginTestNamespace).Delete()
-			Expect(err).ToNot(HaveOccurred(), "error creating test namespace")
+		BeforeEach(func() {
+			var err error
 
-			By("Await module to be deleted")
-			err = await.ModuleObjectDeleted(APIClient, moduleName, kmmparams.DevicePluginTestNamespace, time.Minute)
-			Expect(err).ToNot(HaveOccurred(), "error while waiting module to be deleted")
-
-			svcAccount := serviceaccount.NewBuilder(APIClient, serviceAccountName, kmmparams.DevicePluginTestNamespace)
-			svcAccount.Exists()
-
-			By("Delete ClusterRoleBinding")
-			crb := define.ModuleCRB(*svcAccount, kmodName)
-			err = crb.Delete()
-			Expect(err).ToNot(HaveOccurred(), "error creating test namespace")
-
-			By("Delete Namespace")
-			err = namespace.NewBuilder(APIClient, kmmparams.DevicePluginTestNamespace).Delete()
-			Expect(err).ToNot(HaveOccurred(), "error creating test namespace")
-		})
-		It("should deploy module with a device plugin", reportxml.ID("53678"), func() {
 			By("Create Namespace")
-			testNamespace, err := namespace.NewBuilder(APIClient, kmmparams.DevicePluginTestNamespace).Create()
+			testNamespace, err = namespace.NewBuilder(APIClient, kmmparams.DevicePluginTestNamespace).Create()
 			Expect(err).ToNot(HaveOccurred(), "error creating test namespace")
-
-			configmapContents := define.MultiStageConfigMapContent(kmodName)
-
-			By("Create ConfigMap")
-			dockerfileConfigMap, err := configmap.
-				NewBuilder(APIClient, kmodName, testNamespace.Object.Name).
-				WithData(configmapContents).Create()
-			Expect(err).ToNot(HaveOccurred(), "error creating configmap")
 
 			By("Create ServiceAccount")
-			svcAccount, err := serviceaccount.
+			svcAccount, err = serviceaccount.
 				NewBuilder(APIClient, serviceAccountName, kmmparams.DevicePluginTestNamespace).Create()
 			Expect(err).ToNot(HaveOccurred(), "error creating serviceaccount")
 
 			By("Create ClusterRoleBinding")
-			crb := define.ModuleCRB(*svcAccount, kmodName)
-			_, err = crb.Create()
+			crbBuilder := define.ModuleCRB(*svcAccount, kmodName)
+			_, err = crbBuilder.Create()
 			Expect(err).ToNot(HaveOccurred(), "error creating clusterrolebinding")
+			crb = crbBuilder
+
+			configmapContents := define.MultiStageConfigMapContent(kmodName)
+
+			By("Create ConfigMap")
+			dockerfileConfigMap, err = configmap.
+				NewBuilder(APIClient, kmodName, testNamespace.Object.Name).
+				WithData(configmapContents).Create()
+			Expect(err).ToNot(HaveOccurred(), "error creating configmap")
 
 			By("Create KernelMapping")
 			kernelMapping := kmm.NewRegExKernelMappingBuilder("^.+$")
-
 			kernelMapping.WithContainerImage(image).
 				WithBuildArg(kmmparams.BuildArgName, buildArgValue).
 				WithBuildDockerCfgFile(dockerfileConfigMap.Object.Name)
@@ -91,11 +82,10 @@ var _ = Describe("KMM", Ordered, Label(kmmparams.LabelSuite, kmmparams.LabelSani
 			moduleLoaderContainer := kmm.NewModLoaderContainerBuilder(kmodName)
 			moduleLoaderContainer.WithKernelMapping(kerMapOne)
 			moduleLoaderContainer.WithImagePullPolicy("Always")
-			moduleLoaderContainerCfg, err := moduleLoaderContainer.BuildModuleLoaderContainerCfg()
+			moduleLoaderContainerCfg, err = moduleLoaderContainer.BuildModuleLoaderContainerCfg()
 			Expect(err).ToNot(HaveOccurred(), "error creating moduleloadercontainer")
 
 			By("Create DevicePlugin")
-
 			arch, err := get.ClusterArchitecture(APIClient, GeneralConfig.WorkerLabelMap)
 			if err != nil {
 				Skip("could not detect cluster architecture")
@@ -108,17 +98,52 @@ var _ = Describe("KMM", Ordered, Label(kmmparams.LabelSuite, kmmparams.LabelSani
 			devicePluginImage := fmt.Sprintf(ModulesConfig.DevicePluginImage, arch)
 
 			devicePlugin := kmm.NewDevicePluginContainerBuilder(devicePluginImage)
-			devicePluginContainerCfd, err := devicePlugin.GetDevicePluginContainerConfig()
+			devicePluginContainerCfg, err = devicePlugin.GetDevicePluginContainerConfig()
 			Expect(err).ToNot(HaveOccurred(), "error creating deviceplugincontainer")
+		})
 
+		AfterEach(func() {
+			By("Delete Module")
+			_, err := kmm.NewModuleBuilder(APIClient, moduleName, kmmparams.DevicePluginTestNamespace).Delete()
+			Expect(err).ToNot(HaveOccurred(), "error deleting module")
+
+			By("Await module to be deleted")
+			err = await.ModuleObjectDeleted(APIClient, moduleName, kmmparams.DevicePluginTestNamespace, time.Minute)
+			Expect(err).ToNot(HaveOccurred(), "error while waiting module to be deleted")
+
+			By("Delete ConfigMap")
+			err = dockerfileConfigMap.Delete()
+			Expect(err).ToNot(HaveOccurred(), "error deleting configmap")
+
+			By("Delete ServiceAccount")
+			err = svcAccount.Delete()
+			Expect(err).ToNot(HaveOccurred(), "error deleting serviceaccount")
+
+			By("Delete ClusterRoleBinding")
+			err = crb.Delete()
+			Expect(err).ToNot(HaveOccurred(), "error deleting clusterrolebinding")
+
+			By("Delete Namespace")
+			err = testNamespace.Delete()
+			Expect(err).ToNot(HaveOccurred(), "error deleting namespace")
+
+			By("Wait for namespace to be fully deleted")
+			Eventually(func() bool {
+				_, err := namespace.Pull(APIClient, kmmparams.DevicePluginTestNamespace)
+
+				return err != nil // namespace is deleted when Pull fails
+			}, 2*time.Minute, 5*time.Second).Should(BeTrue(), "namespace was not deleted in time")
+		})
+
+		It("should deploy module with a device plugin", reportxml.ID("53678"), func() {
 			By("Create Module")
 			module := kmm.NewModuleBuilder(APIClient, moduleName, kmmparams.DevicePluginTestNamespace).
 				WithNodeSelector(GeneralConfig.WorkerLabelMap)
 			module = module.WithModuleLoaderContainer(moduleLoaderContainerCfg).
 				WithLoadServiceAccount(svcAccount.Object.Name)
-			module = module.WithDevicePluginContainer(devicePluginContainerCfd).
+			module = module.WithDevicePluginContainer(devicePluginContainerCfg).
 				WithDevicePluginServiceAccount(svcAccount.Object.Name)
-			_, err = module.Create()
+			_, err := module.Create()
 			Expect(err).ToNot(HaveOccurred(), "error creating module")
 
 			By("Await build pod to complete build")
@@ -134,6 +159,7 @@ var _ = Describe("KMM", Ordered, Label(kmmparams.LabelSuite, kmmparams.LabelSani
 			err = await.DeviceDriverDeployment(APIClient, moduleName, kmmparams.DevicePluginTestNamespace, time.Minute,
 				GeneralConfig.WorkerLabelMap)
 			Expect(err).ToNot(HaveOccurred(), "error while waiting on device plugin deployment")
+
 			By("Check module is loaded on node")
 			err = check.ModuleLoaded(APIClient, kmodName, time.Minute)
 			Expect(err).ToNot(HaveOccurred(), "error while checking the module is loaded")
@@ -142,6 +168,41 @@ var _ = Describe("KMM", Ordered, Label(kmmparams.LabelSuite, kmmparams.LabelSani
 			_, err = check.NodeLabel(APIClient, moduleName, kmmparams.DevicePluginTestNamespace,
 				GeneralConfig.WorkerLabelMap)
 			Expect(err).ToNot(HaveOccurred(), "error while checking the module is loaded")
+		})
+
+		It("should redeploy module with a device plugin", reportxml.ID("82674"), func() {
+			By("Create Module")
+			module := kmm.NewModuleBuilder(APIClient, moduleName, kmmparams.DevicePluginTestNamespace).
+				WithNodeSelector(GeneralConfig.WorkerLabelMap)
+			module = module.WithModuleLoaderContainer(moduleLoaderContainerCfg).
+				WithLoadServiceAccount(svcAccount.Object.Name)
+			module = module.WithDevicePluginContainer(devicePluginContainerCfg).
+				WithDevicePluginServiceAccount(svcAccount.Object.Name)
+			_, err := module.Create()
+			Expect(err).ToNot(HaveOccurred(), "error creating module")
+
+			By("Await build pod to complete build")
+			err = await.BuildPodCompleted(APIClient, kmmparams.DevicePluginTestNamespace, 5*time.Minute)
+			Expect(err).ToNot(HaveOccurred(), "error while building module")
+
+			By("Await driver container deployment")
+			err = await.ModuleDeployment(APIClient, moduleName, kmmparams.DevicePluginTestNamespace, time.Minute,
+				GeneralConfig.WorkerLabelMap)
+			Expect(err).ToNot(HaveOccurred(), "error while waiting on driver deployment")
+
+			By("Await device driver deployment")
+			err = await.DeviceDriverDeployment(APIClient, moduleName, kmmparams.DevicePluginTestNamespace, time.Minute,
+				GeneralConfig.WorkerLabelMap)
+			Expect(err).ToNot(HaveOccurred(), "error while waiting on device plugin deployment")
+
+			By("Check module is loaded on node")
+			err = check.ModuleLoaded(APIClient, kmodName, time.Minute)
+			Expect(err).ToNot(HaveOccurred(), "error while checking the module is loaded")
+
+			By("Check label is set on all nodes")
+			_, err = check.NodeLabel(APIClient, moduleName, kmmparams.DevicePluginTestNamespace,
+				GeneralConfig.WorkerLabelMap)
+			Expect(err).ToNot(HaveOccurred(), "error while checking node labels")
 		})
 	})
 
