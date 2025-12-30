b6fb43bcaed02e5f3ac7f4aca5659660124e0bff
Guygored <ggordani@redhat.com>
2025-12-30 12:31:12 +0200

    NFD upgrade test: migrate to new deployer and fix await polling
    - Migrated upgrade test from NfdAPIResource to OperatorInstaller (same as feature tests)
    - Added wait for CSV before creating NFD CR
    - Fixed await.OperatorUpgrade to continue polling instead of returning error immediately
    - Fixed version regex pattern to match exact version

--- tests/hw-accel/nfd/2upgrade/internal/await/await.go
+++ tests/hw-accel/nfd/2upgrade/internal/await/await.go
@@ -2,7 +2,6 @@ package await
 
 import (
 	"context"
-	"fmt"
 	"regexp"
 	"time"
 
@@ -20,7 +19,7 @@ func OperatorUpgrade(apiClient *clients.Settings, versionRegex string, timeout t
 		context.TODO(), time.Second, timeout, true, func(ctx context.Context) (bool, error) {
 			regex := regexp.MustCompile(versionRegex)
 
-			csv, err := olm.ListClusterServiceVersionWithNamePattern(apiClient, "nfd",
+			csv, _ := olm.ListClusterServiceVersionWithNamePattern(apiClient, "nfd",
 				nfdparams.NFDNamespace)
 
 			for _, csvResource := range csv {
@@ -32,17 +31,16 @@ func OperatorUpgrade(apiClient *clients.Settings, versionRegex string, timeout t
 				csvVersion := csvResource.Object.Spec.Version.String()
 				matched := regex.MatchString(csvVersion)
 
-				klog.V(nfdparams.LogLevel).Infof("csvVersion %v is matched:%v with regex%v", csvVersion, matched, versionRegex)
+				klog.V(nfdparams.LogLevel).Infof("csvVersion %v is matched:%v with regex %v", csvVersion, matched, versionRegex)
 
 				if matched {
 					return csvResource.Object.Status.Phase == "Succeeded", nil
 				}
 			}
 
-			if err == nil {
-				err = fmt.Errorf("csv with version pattern %v not found", versionRegex)
-			}
+			// CSV not found yet - continue polling (return nil to keep waiting)
+			klog.V(nfdparams.LogLevel).Infof("CSV with version pattern %v not found yet, continuing to wait...", versionRegex)
 
-			return false, err
+			return false, nil
 		})
 }
--- tests/hw-accel/nfd/2upgrade/tests/upgrade-test.go
+++ tests/hw-accel/nfd/2upgrade/tests/upgrade-test.go
@@ -8,11 +8,11 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/olm"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/reportxml"
-	nfdDeploy "github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/internal/deploy"
-	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/internal/hwaccelparams"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/internal/deploy"
 	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/2upgrade/internal/await"
 	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/2upgrade/internal/tsparams"
-	NfdConfig "github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/internal/nfdconfig"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/internal/nfdconfig"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/internal/nfdhelpers"
 	"github.com/rh-ecosystem-edge/eco-gotests/tests/hw-accel/nfd/nfdparams"
 	. "github.com/rh-ecosystem-edge/eco-gotests/tests/internal/inittools"
 	"k8s.io/klog/v2"
@@ -21,51 +21,84 @@ import (
 var _ = Describe("NFD", Ordered, Label(nfdparams.Label), func() {
 
 	Context("Operator", Label(tsparams.NfdUpgradeLabel), func() {
-		nfdConfig := NfdConfig.NewNfdConfig()
+		nfdConfig := nfdconfig.NewNfdConfig()
 
-		nfdManager := nfdDeploy.NewNfdAPIResource(APIClient,
-			hwaccelparams.NFDNamespace,
-			"op-nfd",
-			"nfd",
-			nfdConfig.CatalogSource,
-			"openshift-marketplace",
-			"nfd",
-			"stable")
+		var nfdInstaller *deploy.OperatorInstaller
+		var nfdCRUtils *deploy.NFDCRUtils
 
 		BeforeAll(func() {
 			if nfdConfig.CatalogSource == "" {
 				Skip("No CatalogSourceName defined. Skipping test")
 			}
-			By("Creating nfd")
-			err := nfdManager.DeployNfd(25*int(time.Minute), false, "")
-			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error in deploy NFD %s", err))
+
+			var options *nfdhelpers.NFDInstallConfigOptions
+			if nfdConfig.CatalogSource != "" {
+				options = &nfdhelpers.NFDInstallConfigOptions{
+					CatalogSource: nfdhelpers.StringPtr(nfdConfig.CatalogSource),
+				}
+			}
+
+			installConfig := nfdhelpers.GetDefaultNFDInstallConfig(APIClient, options)
+			nfdInstaller = deploy.NewOperatorInstaller(installConfig)
+			nfdCRUtils = deploy.NewNFDCRUtils(APIClient, installConfig.Namespace, nfdparams.NfdInstance)
+
+			By("Installing NFD operator")
+			err := nfdInstaller.Install()
+			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error installing NFD operator: %s", err))
+
+			By("Waiting for NFD operator CSV to be ready")
+			ready, err := nfdhelpers.WaitForNFDOperatorReady(APIClient, 5*time.Minute)
+			Expect(err).ToNot(HaveOccurred(), "error waiting for NFD operator CSV")
+			Expect(ready).To(BeTrue(), "NFD operator CSV not ready")
+
+			By("Creating NFD CR")
+			crConfig := deploy.NFDCRConfig{
+				Image: nfdConfig.Image,
+			}
+			err = nfdCRUtils.DeployNFDCR(crConfig)
+			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error creating NFD CR: %s", err))
+
+			By("Waiting for NFD CR to be ready")
+			crReady, err := nfdCRUtils.IsNFDCRReady(5 * time.Minute)
+			Expect(err).ToNot(HaveOccurred(), "error waiting for NFD CR")
+			Expect(crReady).To(BeTrue(), "NFD CR not ready")
 		})
+
 		AfterAll(func() {
-			By("Undeploy NFD instance")
-			err := nfdManager.UndeployNfd(tsparams.NfdInstance)
-			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error in undeploy NFD %s", err))
+			By("Deleting NFD CR")
+			err := nfdCRUtils.DeleteNFDCR()
+			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error deleting NFD CR: %s", err))
 
+			By("Uninstalling NFD operator")
+			uninstallConfig := nfdhelpers.GetDefaultNFDUninstallConfig(
+				APIClient,
+				"nfd-operator-group",
+				"nfd-subscription")
+			nfdUninstaller := deploy.NewOperatorUninstaller(uninstallConfig)
+			err = nfdUninstaller.Uninstall()
+			Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("error uninstalling NFD: %s", err))
 		})
-		It("should upgrade successfully", reportxml.ID("54540"), func() {
 
+		It("should upgrade successfully", reportxml.ID("54540"), func() {
 			if nfdConfig.UpgradeTargetVersion == "" {
-				Skip("No UpgradeTargetVersion defined. Skipping test ")
+				Skip("No UpgradeTargetVersion defined. Skipping test")
 			}
 			if nfdConfig.CustomCatalogSource == "" {
-				Skip("No CustomCatalogSource defined. Skipping test ")
+				Skip("No CustomCatalogSource defined. Skipping test")
 			}
+
 			By("Getting NFD subscription")
-			sub, err := olm.PullSubscription(APIClient, "nfd", nfdparams.NFDNamespace)
+			sub, err := olm.PullSubscription(APIClient, "nfd-subscription", nfdparams.NFDNamespace)
 			Expect(err).ToNot(HaveOccurred(), "failed getting subscription")
 
 			By("Update subscription to use new catalog source")
-			klog.V(nfdparams.LogLevel).Infof("SUB: %s", sub.Object.Spec.CatalogSource)
+			klog.V(nfdparams.LogLevel).Infof("Current CatalogSource: %s", sub.Object.Spec.CatalogSource)
 			sub.Definition.Spec.CatalogSource = nfdConfig.CustomCatalogSource
 			_, err = sub.Update()
 			Expect(err).ToNot(HaveOccurred(), "failed updating subscription")
 
 			By("Await operator to be upgraded")
-			versionRegexPattern := fmt.Sprintf(`(%s)\S+`, nfdConfig.UpgradeTargetVersion)
+			versionRegexPattern := fmt.Sprintf(`%s`, nfdConfig.UpgradeTargetVersion)
 			err = await.OperatorUpgrade(APIClient, versionRegexPattern, 10*time.Minute)
 			Expect(err).ToNot(HaveOccurred(), "failed awaiting subscription upgrade")
 		})
