971a981a9ce58299acf7e37f2de0694d24993218
Joshua Clark <josclark@.redhat.com>
2026-01-15 08:13:03 -0500

    Added SeedGeneration helper to LCA test suite

--- tests/lca/internal/lcaparams/const.go
+++ tests/lca/internal/lcaparams/const.go
@@ -3,4 +3,7 @@ package lcaparams
 const (
 	// Label represents lca label that can be used for test cases selection.
 	Label = "lca"
+
+	// LCALogLevel custom loglevel for the lca testing verbose mode.
+	LCALogLevel = 90
 )
