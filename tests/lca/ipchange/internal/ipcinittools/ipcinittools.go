61cf283714f286952df913e83dfd5be07de89d92
Alexander Chuzhoy <achuzhoy@redhat.com>
2026-03-11 10:36:58 -0400

    lca ipc: adding automation for 4 validation testcases (#1218)
    Will validate 4 fields of the ipc:
    1. stage
    2. vlan
    3. initMonitorTimeoutSeconds
    4. ipv4 address
    
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

---
+++ tests/lca/ipchange/internal/ipcinittools/ipcinittools.go
@@ -0,0 +1,23 @@
+package ipcinittools
+
+import (
+	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/clients"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/internal/inittools"
+	"github.com/rh-ecosystem-edge/eco-gotests/tests/lca/ipchange/internal/ipcconfig"
+)
+
+var (
+	// APIClient is the API client for the cluster (from ECO_LCA_IPC_KUBECONFIG_TARGET_SNO).
+	APIClient *clients.Settings
+	// IPCConfig provides access to configuration parameters.
+	IPCConfig *ipcconfig.IPCConfig
+)
+
+// init loads all variables automatically when this package is imported. Once package is imported a user has full
+// access to all vars within init function. It is recommended to import this package using dot import.
+//
+//nolint:gochecknoinits // Package initialization pattern used throughout eco-gotests
+func init() {
+	IPCConfig = ipcconfig.NewIPCConfig()
+	APIClient = inittools.APIClient
+}
