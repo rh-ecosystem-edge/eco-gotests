ebb3b54c4097c78c5fec34efdaca735baee465c3
Joshua Clark <josclark@.redhat.com>
2026-03-17 09:42:37 -0400

    :Modified IBU AfterEach to fix race condition in rollback test

--- tests/lca/imagebasedupgrade/cnf/upgrade-talm/tests/rollback-after-failed-upgrade-test.go
+++ tests/lca/imagebasedupgrade/cnf/upgrade-talm/tests/rollback-after-failed-upgrade-test.go
@@ -64,11 +64,9 @@ var _ = Describe(
 
 				// Check the Idle condition status
 				isIdle := false
-
 				for _, condition := range ibu.Object.Status.Conditions {
 					if condition.Type == "Idle" && condition.Status == "True" {
 						isIdle = true
-
 						break
 					}
 				}
