709ea600a9134d22ace30584803e0860eb0f977e
Alexander Chuzhoy <achuzhoy@redhat.com>
2026-03-23 17:13:43 -0400

    lca ipc: automating test for auto rollback
    This tests that the rollback occurs after a timeout.
    
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

--- tests/lca/ipchange/tests/ipconfig-test.go
+++ tests/lca/ipchange/tests/ipconfig-test.go
@@ -2,6 +2,7 @@ package ipchange_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -115,4 +116,70 @@ var _ = Describe(
 					return err
 				}),
 		)
+
+		It("Validates AutoRollbackOnFailure works as expected", reportxml.ID("88105"), func() {
+			By("Pulling IPConfig")
+
+			builder, err := lca.PullIPConfig(APIClient)
+			Expect(err).NotTo(HaveOccurred(), "failed to pull IPConfig")
+			Expect(builder).NotTo(BeNil(), "IPConfig builder is nil")
+
+			By("Updating AutoRollbackOnFailure")
+
+			builder.Definition.Spec.AutoRollbackOnFailure = &lcaipcv1.AutoRollbackOnFailure{
+				InitMonitorTimeoutSeconds: 9,
+			}
+
+			By("Updating IPv4 fields with arbitrary values to ensure the spec is updated")
+
+			builder.WithIPv4Address("192.168.250.250").
+				WithIPv4Gateway("192.168.250.254").
+				WithIPv4MachineNetwork("192.168.250.0/24")
+
+			By("Setting the stage to Config")
+
+			builder.WithStage(string(lcaipcv1.IPStages.Config))
+
+			By("Updating IPConfig")
+
+			_, err = builder.Update()
+			Expect(err).NotTo(HaveOccurred(), "failed to update AutoRollbackOnFailure")
+
+			By("Waiting for IPConfig to fail")
+
+			builder, err = builder.WaitUntilFailed(time.Minute * 15)
+			Expect(err).NotTo(HaveOccurred(), "failed to wait for IPConfig to rollback")
+
+			By("Checking the IPConfig status to contain the rollback message")
+
+			Expect(builder.Object.Status.Conditions).To(ContainElement(
+				HaveField("Message", ContainSubstring("Rollback due to LCA Init Monitor timeout")),
+			))
+
+			By("Waiting for IPConfig to allow Idle stage")
+
+			Eventually(func() (bool, error) {
+				builder, err = lca.PullIPConfig(APIClient)
+				if err != nil {
+					return false, err
+				}
+
+				return builder.Object.Status.ValidNextStages[0] == "Idle", nil
+			}).WithTimeout(time.Minute*3).WithPolling(time.Second*10).Should(
+				BeTrue(), "error waiting for ipconfig to allow Idle stage")
+
+			By("Move the stage back to Idle")
+
+			builder.WithStage(string(lcaipcv1.IPStages.Idle))
+
+			By("Updating IPConfig")
+
+			_, err = builder.Update()
+			Expect(err).NotTo(HaveOccurred(), "failed to set stage to Idle for IPConfig")
+
+			By("Waiting for IPConfig to become Idle")
+
+			_, err = builder.WaitUntilIdle(time.Minute * 5)
+			Expect(err).NotTo(HaveOccurred(), "failed to wait for IPConfig to become Idle")
+		})
 	})
