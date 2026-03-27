6c9494a9b1776a5c36793af5010683ce7b47bf95
Kirsten Laskoski <klaskosk@redhat.com>
2026-03-27 13:38:57 -0400

    deps: update golangci-lint
    This commit updates golangci-lint from 2.9.0 to 2.11.4.

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -4,7 +4,7 @@ set -e
 
 . "$(dirname "$0")"/common.sh
 
-GOLANGCI_LINT_VERSION="2.9.0"
+GOLANGCI_LINT_VERSION="2.11.4"
 
 # IsGoLangCiLintInstalled is used to check whether golangci-lint executable is on the $PATH.
 function IsGolangCiLintInstalled() {
