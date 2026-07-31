6e814b2092f939b82a8a4f0ff892da03b288aa86
Keerthana Chennagoni <kchennag@redhat.com>
2026-07-31 08:19:55 -0500

    CNF-24974 Move disk partitioning tests to rdscore system tests

--- tests/cnf/core/diskpartitioning/internal/tsparams/diskpartitioningvars.go
+++
@@ -1,6 +0,0 @@
-package tsparams
-
-import "github.com/rh-ecosystem-edge/eco-gotests/tests/cnf/core/internal/coreparams"
-
-// Labels represents the range of labels that can be used for test case selection.
-var Labels = append(coreparams.Labels, LabelSuite)
